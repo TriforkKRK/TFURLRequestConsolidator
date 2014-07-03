0b9d50b5934480d4fdd9e46c79dc4c5dfccfc3df
Krzysztof Profic <kprofic@gmail.com>
2014-07-03 16:07:34 +0200

    Initial implementation added

---
+++ tfurlrequestconsolidator/tfurlrequestconsolidator/TFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TFAppDelegate.h
+//  tfurlrequestconsolidator
+//
+//  Created by Krzysztof Profic on 03/07/14.
+//  Copyright (c) 2014 Trifork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
